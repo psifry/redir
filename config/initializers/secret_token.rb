e065e1d4743c4e94ff4df1988e0820430360265e
unknown <fry01@LAGBCHCH00-0041.springer-sbm.com>
2012-07-20 00:34:24 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redir::Application.config.secret_token = '7cba0f9ff0a95b1c36c4f55d1842cfe5297e51ec08475131cdb4483c3d4a15f69a48a216e32aa485efcf4f8dc5ec247f92d5c91ffcdafa8d9d0f3f6a42b5d0f3'
